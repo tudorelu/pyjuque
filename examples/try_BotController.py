de3257cbe32dc38b8294fac7ebbe209d98560029
pimpmypixel <pimpmypixel@me.com>
2021-01-04 14:57:46 +0100

    add Dockerfile

--- examples/try_BotController.py
+++ examples/try_BotController.py
@@ -1,31 +1,29 @@
 import os
 import sys
-curr_path = os.path.abspath(__file__)
-root_path = os.path.abspath(
-    os.path.join(curr_path, os.path.pardir, os.path.pardir))
-sys.path.append(root_path)
-
 import time
-
+curr_path = os.path.abspath(__file__)
+root_path = os.path.abspath(os.path.join(curr_path, os.path.pardir, os.path.pardir))
+sys.path.append(root_path)
+from pprint import pprint
+from yaspin import yaspin
 from pyjuque.Engine.Models import Base, Bot, Order, Pair, EntrySettings, ExitSettings, getSession
 from pyjuque.Engine.BotController import BotController
-from pprint import pprint
-
 from pyjuque.Strategies.EMAXStrategy import EMACrossover
 from pyjuque.Strategies.BBRSIStrategy import BBRSIStrategy
 from pyjuque.Strategies.AlwaysBuyStrategy import AlwaysBuyStrategy
-
 from pyjuque.Exchanges.Binance import Binance
 
-from yaspin import yaspin
-
 time_to_sleep = 10
+bot_name = 'bot1'
+db_name = 'db.db'
+symbols = ['ETHBTC', 'ZILBTC', 'BTCUSDT', 'MKRBTC','ETCBTC','SNXBTC']
+
 
 def initialize_database(session, symbols=[]):
     """ Function that initializes the database
     by creating a bot with two pairs. """
     myobject = Bot(
-        name="test_bot_tudor",
+        name=bot_name,
         quote_asset = 'BTC',
         starting_balance = 0.001,
         current_balance = 0.001,
@@ -51,6 +49,7 @@ def initialize_database(session, symbols=[]):
     myobject.entry_settings = entrysets
     myobject.exit_settings = exitsets
     session.commit()
+
     for symbol in symbols:
         pair = Pair(
             bot_id = myobject.id,
@@ -58,32 +57,29 @@ def initialize_database(session, symbols=[]):
             current_order_id = None
         )
         session.add(pair)
+
     session.commit()
 
 def Main():
     resetOrdersPairs = False
-    session = getSession('sqlite:///pyjuque_live_5.db')
-
+    session = getSession('sqlite:///' + db_name)
     exchange = Binance(get_credentials_from_env=True)
-
-    symbols = ['ETHBTC', 'YFIBTC', 'UNIBTC']
+    
+    # Add all symbols on exchange
     # for symbol in exchange.SYMBOL_DATAS.keys():
     #     if exchange.SYMBOL_DATAS[symbol]["status"] == "TRADING" \
     #         and exchange.SYMBOL_DATAS[symbol]["quoteAsset"] == "BTC":
     #         symbols.append(symbol)
 
     # First time you run this, uncomment the next line
-    # initialize_database(session, symbols)
+    #initialize_database(session, symbols)
 
-    bot = session.query(Bot).filter_by(name='test_bot_tudor').first()
-    # input your path to credentials here.
+    bot = session.query(Bot).filter_by(name=bot_name).first()
+    # strategy = AlwaysBuyStrategy()
+    strategy = BBRSIStrategy(13, 40, 70, 30)
 
-    strategy = AlwaysBuyStrategy()
-    # strategy = BBRSIStrategy(13, 40, 70, 30)
     bot_controller = BotController(session, bot, exchange, strategy)
-
     sp = yaspin()
-
     bot_controller.sp = sp
     bot_controller.sp_on = True
     while True:
@@ -99,4 +95,4 @@ def Main():
             left_to_sleep -= 1
 
 if __name__ == '__main__':
-        Main()
+    Main()
